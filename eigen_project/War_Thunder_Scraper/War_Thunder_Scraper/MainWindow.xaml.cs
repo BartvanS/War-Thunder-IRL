a284ecc76da4f69c30a1b2add2858f9fd77b3d2b
Bart van Sevenbergen <bartvansevenbergen@hotmail.com>
2020-02-16 18:21:31 +0100

    scrape war thunder date and write to serial

--- eigen_project/War_Thunder_Scraper/War_Thunder_Scraper/MainWindow.xaml.cs
+++ eigen_project/War_Thunder_Scraper/War_Thunder_Scraper/MainWindow.xaml.cs
@@ -3,21 +3,12 @@ using Newtonsoft.Json.Linq;
 using ScrapySharp.Extensions;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO.Ports;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Script.Serialization;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using War_Thunder_Scraper.classes;
+
 
 namespace War_Thunder_Scraper
 {
@@ -29,14 +20,49 @@ namespace War_Thunder_Scraper
         public MainWindow()
         {
             InitializeComponent();
+            setup();
         }
-
+        PortConnection connection;
+        private void setup()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                availablePorts.Items.Add(port);
+            }
+        }
         private void startScraping_Click(object sender, RoutedEventArgs e)
         {
             string json = new WebClient().DownloadString("http://127.0.0.1:8111/state");
             string correctJson = json.Replace("TAS, km/h", "tas");
             dynamic data = JObject.Parse(correctJson);
             Console.WriteLine(data.tas);
+            connection.writeToSerial(Convert.ToString(data.tas));
         }
+
+        private void disconnectPort_Click(object sender, RoutedEventArgs e)
+        {
+            if (connection.Sp.IsOpen)
+            {
+                connection.closePort();
+            }
+        }
+
+
+        private void connectPort_Click(object sender, RoutedEventArgs e)
+        {
+            initializePort();
+        }
+
+        private void initializePort()
+        {
+            if (availablePorts.HasItems)
+            {
+                string portName = availablePorts.Items[availablePorts.SelectedIndex].ToString();
+                connection = new PortConnection(portName, 9600, 500, 500);
+            }
+        }
+
+
     }
 }
---
+++ eigen_project/War_Thunder_Scraper/War_Thunder_Scraper/classes/PortConnection.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace War_Thunder_Scraper.classes
+{
+    class PortConnection
+    {
+        public SerialPort Sp { get; set; }
+        public string PortName { get; set; }
+        public int BaudRate { get; set; }
+        public int ReadTimeout { get; set; }
+        public int WriteTimeout { get; set; }
+
+        public PortConnection(string portName, int baudRate, int readTimeout, int writeTimeout)
+        {
+            this.Sp = new SerialPort();
+            this.PortName = portName;
+            this.BaudRate = baudRate;
+            this.ReadTimeout = readTimeout;
+            this.WriteTimeout = writeTimeout;
+            this.setupConnection();
+        }
+
+        private void setupConnection()
+        {
+            this.Sp.PortName = this.PortName;
+            this.Sp.BaudRate = this.BaudRate;
+            this.Sp.ReadTimeout = this.ReadTimeout;
+            this.Sp.WriteTimeout = this.WriteTimeout;
+            this.Sp.Open();
+        }
+        public void writeToSerial(string value)
+        {
+            //string parsedValue = "%" + value + "#";
+            Console.WriteLine(Convert.ToString(this.ReadTimeout) + " + " + Convert.ToString(this.WriteTimeout) + " + " + this.PortName + " + " + Convert.ToString(this.BaudRate));
+            Sp.Write(value);
+        }
+
+        public void closePort()
+        {
+            Console.WriteLine("Port closed");
+            Sp.Close();
+        }
+
+    }
+}

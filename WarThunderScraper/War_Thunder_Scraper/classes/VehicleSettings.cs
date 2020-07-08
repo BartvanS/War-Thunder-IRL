7384e66e3173acb2b47409a7b4d3eff05301e719
Bart van Sevenbergen <bartvansevenbergen@hotmail.com>
2020-07-08 12:20:35 +0200

    .

---
+++ WarThunderScraper/War_Thunder_Scraper/classes/VehicleSettings.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WarThunderScraper.classes;
+
+namespace War_Thunder_Scraper.classes
+{
+    public class VehicleSettings
+    {
+        /// <summary>
+        /// Retrieves all data available and sends it to the arduino. This is default
+        /// </summary>
+        // public bool retrieveAllData = true;
+
+
+        /// <summary>
+        /// If you want te select what data to use, you can manually select it.
+        /// </summary>
+
+        public bool RetrieveSpeed { get; set; } = false;
+
+        public bool RetrieveHeight { get; set; } = false;
+
+        public bool UseTestData { get; set; } = false;
+        public ArduinoActions.VehicleType VehicleType { get; set; } = ArduinoActions.VehicleType.Plane;
+
+        public VehicleSettings()
+        {
+        }
+
+        public VehicleSettings(bool retrieveSpeed, bool retrieveHeight, bool useTestData, ArduinoActions.VehicleType vehicleType)
+        {
+            RetrieveSpeed = retrieveSpeed;
+            RetrieveHeight = retrieveHeight;
+            UseTestData = useTestData;
+            VehicleType = vehicleType;
+        }
+    }
+}
